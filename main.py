069cc5abc134cb1f1990516f36e9d61899a3a31a
Xulian7 <jfgrossop@outlook.com>
2025-09-28 15:11:30 -0500

    Primera version Corr

--- main.py
+++ main.py
@@ -65,8 +65,7 @@ async def login(data: LoginData):
 
 # Puerto dinámico para Railway
 if __name__ == "__main__":
-    import os
-    import uvicorn
+
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
